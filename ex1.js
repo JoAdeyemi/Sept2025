119f524c822f11e148a4ba5b1ff39a2f605c9f78
JoAdeyemi <adeyemiolufemi_j@yahoo.com>
2025-10-07 14:33:23 +0100

    ex1.js

--- ex1.js
+++ ex1.js
@@ -1,12 +1,11 @@
-document.getElementById("weight").addEventListener("click",weight);
 function check() {
-            let weight = parseInt(document.getElementById("weight").value);
-            if (isNaN(weight)) {
-                alert("Please enter a valid number for baggage weight.");
-            } else if (weight <= 50) {
-                alert("Baggage weight is " + weight + "kg is Allowed.");
-            } else {
-                alert("Baggage weight is " + weight + "kg is Overweight!");
-                document.getElementById("result").textContent = "weight"
-            }
-        }
+    let weight = parseInt(document.getElementById("weight").value);
+
+    if (isNaN(weight)) {
+        alert("Please enter a valid number for baggage weight.");
+    } else if (weight <= 50) {
+        alert("Baggage weight is " + weight + "kg is Allowed.");
+    } else {
+        alert("Baggage weight is " + weight + "kg is Overweight!");
+    }
+}
