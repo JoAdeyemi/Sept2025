56fc2e1e4118b4e8d0167e8ab81a71a7b73211be
JoAdeyemi <adeyemiolufemi_j@yahoo.com>
2025-10-07 19:00:26 +0100

    ex1.js

--- ex2.js
+++ ex2.js
@@ -1,16 +1,3 @@
-function check() {
-    let weight = parseInt(document.getElementById("weight").value);
-
-    if (isNaN(weight)) {
-        alert("Please enter a valid number for baggage weight.");
-    } else if (weight <= 50) {
-        alert("Baggage weight is " + weight + "kg. Allowed.");
-    } else {
-        alert("Baggage weight is " + weight + "kg. Overweight!");
-        handleExcess(weight);
-    }
-}
-
 // Function to handle overweight baggage
 function handleExcess(weight) {
     let excess = weight - 50;
