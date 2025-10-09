005ca0582de95d07f3a311d2bb1004d6f4acdab9
JoAdeyemi <adeyemiolufemi_j@yahoo.com>
2025-10-09 11:02:36 +0100

    ex1.js

--- Ex3.js
+++ Ex3.js
@@ -18,3 +18,15 @@
       document.getElementById("result").innerText =
       `The sum of all multiples of ${num1} or ${num2} below ${limit} is ${sum}.`;
     }
+
+ function check() {
+    let weight = parseInt(document.getElementById("weight").value);
+
+    if (isNaN(weight)) {
+        alert("Please enter a valid number for baggage weight.");
+    } else if (weight <= 50) {
+        alert("Baggage weight is " + weight + "kg is Allowed.");
+    } else {
+        alert("Baggage weight is " + weight + "kg is Overweight!");
+    }
+}
