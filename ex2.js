973de65a5463acfb5e7d3cc7665fb6508ce46108
JoAdeyemi <adeyemiolufemi_j@yahoo.com>
2025-10-11 11:16:25 +0100

    ex1.js

--- ex2.js
+++ ex2.js
@@ -1,7 +1,7 @@
 function checkBaggage() {
   // Get the weight entered by the user
   let weight = parseFloat(document.getElementById("weight").value);
-  let result = document.getElementById("result");
+  let result = document.getElementById("size");
 
   // Validate input
   if (isNaN(weight)) {
